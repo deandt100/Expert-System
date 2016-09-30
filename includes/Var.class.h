9404fdc93f27abe474f7b0f6815b8cb80dc3345b
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-09-30 08:50:46 +0200

    added name to Var

--- includes/Var.class.h
+++ includes/Var.class.h
@@ -5,12 +5,14 @@ class Var
 	private:
 		bool	state;
 		bool	isFinal;
+		char	name;
 	public:
 		void	setState(bool state){ this->state = state; }
 		void	setFinal(bool isFinal){ this->isFinal = isFinal; }
 		bool	getState(){ return (state); }
 		bool	getFinal(){ return (state); }
+		char	getName(){ return (name); }
 
 	//Construct
-	Var(bool);
+	Var(bool, char);
 };
--- src/Var.class.cpp
+++ src/Var.class.cpp
@@ -1,7 +1,8 @@
 #include "Var.class.h"
 
-Var::Var(bool state)
+Var::Var(bool state, char name)
 {
 	this->state = state;
 	this->state = false;
+	this->name = name;
 }
